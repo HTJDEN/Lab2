a401fd0890e0fc7ffd51c591a7fb7b8c8bf442cf
HTJDEN <dennishotj.24@ichat.sp.edu.sg>
2025-04-29 15:40:37 +0800

    added new bmi file

---
+++ bmi.py
@@ -0,0 +1,34 @@
+def calculate_bmi(height, weight):
+
+    print("height " , height)
+    print("weight " + str(weight))
+    bmi = weight/(height**2)
+    print("your bmi is", bmi)
+    return bmi
+
+def classify_bmi(bmi):
+    if(bmi < 18.5):
+        print("you are underweight")
+    elif (bmi >= 18.5 and bmi <=25):
+        print("you are acceptable weight")
+    else:
+        print("you are overweight")
+    return 
+
+def app():
+    output = calculate_bmi (1.7 , 60)
+    classify_bmi(output)
+    return 
+
+if __name__ == "__main__":
+    app()
+
+
+
+
+
+
+
+
+
+
