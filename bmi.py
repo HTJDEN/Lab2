8590bb7904b696c2dc1ee33750d507b5d54b422f
HTJDEN <dennishotj.24@ichat.sp.edu.sg>
2025-05-06 15:33:39 +0800

    added new return values

--- bmi.py
+++ bmi.py
@@ -8,12 +8,14 @@ def calculate_bmi(height, weight):
 
 def classify_bmi(bmi):
     if(bmi < 18.5):
-        print("you are underweight")
+        # print("you are underweight")
+        return 1
     elif (bmi >= 18.5 and bmi <=25):
-        print("you are acceptable weight")
+        # print("you are acceptable weight")
+        return 0
     else:
-        print("you are overweight")
-    return 
+        # print("you are overweight")
+    return -1
 
 def app():
     output = calculate_bmi (1.7 , 60)
